5198ed30cff44076c8512055f355a6020b2ee098
Marcus Larsen <marcuslarsen97@gmail.com>
2024-02-08 18:37:55 +0100

    Create simple-calculator.py

---
+++ projects/simple-calculator.py
@@ -0,0 +1,65 @@
+""" Simple calculator
+A simple calculator for simple expressions.
+First it takes a binary operator like +, -, * or / and then the two
+operands (x and y) to calculate simple expressions like x + y and x / y.
+
+The program might look something like this:
+
+	This is a simple calculator.
+	First you input the operator, then the operands one at a time.
+	
+	Input operator [+-*/]
+	> +
+	Input x in (x + y)
+	> 19
+	Input y in (19 + y)
+	> 3.4
+	Result:
+	19 + 3.4 = 22.4
+
+
+-- STEPS --
+Follow the steps underneath to create the program and afterwards implement
+the extensions. The steps also serve as a structure for the program.
+Feel free to change or add anything you like :)
+
+1. get operator [+-*/] from user
+2. get operands (x and y) from user
+3. calculate result
+4. print result
+
+
+-- EXTENSIONS --
+These are ideas for extending the program.
+If you have any ideas of your own feel free to go ahead with those.
+
+- Handle errors, for example invalid input and division by zero.
+- Implement a loop to allow for multiple calculations after
+  the first one is done.
+- Save the result from the last calculation in a variable. Now make this
+  previous result accessible with a special symbol in the operand input stage.
+  This way you can chain together calculations.
+- Implement support for more operators, for example exponentation (^)
+  and square root (√).
+- Implement support for functions, for example sin() or log().
+"""
+
+
+# Step 1: Get operator from the user.
+# These include [+-*/]
+
+
+
+# Step 2: Get operands from the user
+# These are the x and y in an expression like x + y
+
+
+
+# Step 3: Calculate the result
+# Use the operator and the operands to calculate the result
+
+
+
+# Step 4: Print the result
+# Print the result in a nicely formatted way
+
